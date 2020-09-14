5a564347313ea555e900fc332af90314f91824d7
Davis Kane <daviskane@MacBook-Pro.local>
2020-09-14 17:07:28 -0400

    Build a React App 1  - completed

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import './Card.css';
 
-const Card = (props) => {
+const Card = ({ name, email, id }) => {
     return (
-        <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='' src='https://robohash.org/test?100x100' />
+        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <img alt='' src={`https://robohash.org/${id}?size=200x200`} />
             <div>
-                <h2>{props.name}</h2>
-                <p2>{props.email}</p2>
+                <h2>{name}</h2>
+                <p2>{email}</p2>
             </div>
         </div>
     )
-}
+};
 
 export default Card;
